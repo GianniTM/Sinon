4dea2021e7ba78fc1b5c974f5ccccae03a03f2ef
Jens Hermans <r0739255@thomasmore.be>
2020-05-21 22:10:28 +0200

    kick aangepast

--- bot.js
+++ bot.js
@@ -33,12 +33,11 @@ client.on('message', message => {
     else if (command === '/kick') {
         // grab the "first" mentioned user from the message
         // this will return a User object, just like message.author
-        if (!message.mentions.users.size) {
-            return message.reply('you need to tag a user in order to kick them!');
-        }
-        const taggedUser = message.mentions.users.first();
 
+        const taggedUser = message.mentions.users.first();
         message.channel.send(You wanted to kick: ${taggedUser.username});
+
+
     }
 
 });
