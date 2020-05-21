2c0151dc6ae2ae15436fc5dde929844990d56fba
Gianni Andries <45100198+GianniTM@users.noreply.github.com>
2020-05-21 21:06:31 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,12 +3,25 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log('I am ready!');
+     client.user.setStatus('available')
+     client.user.setPresence({
+        game: {
+            name: 'With Asada',
+            type: "STREAMING",
+            url: "https://www.twitch.tv/xealiusrl"
+        }
+    });
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
+    if (message.content === '/Q') {
+    	message.channel.send({files: ["Images/sinon1.jpg"]});
   	}
+    else if (message.content === '/q'){
+        message.channel.send({files: ["https://i.pinimg.com/originals/b1/66/e8/b166e84e5f01e9ec56bc0f61a8ea940c.jpg"]});
+    }
+    else if (message.content === '/help'){
+	message.channel.send("```diff\n-Help```");
 });
 
 // THIS  MUST  BE  THIS  WAY
