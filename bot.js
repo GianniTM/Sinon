1bb44f9d58acc7baa29a2e50a033e4d2e94fce9f
Gianni Andries <45100198+GianniTM@users.noreply.github.com>
2020-05-21 18:17:41 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,9 +6,12 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.channel.send("My Bot's message", {files: ["https://i.pinimg.com/originals/48/9e/93/489e93437a3209ae36a2583c7ce562ad.jpg"]});
+    if (message.content === 'Quote') {
+    	message.channel.send({files: ["https://i.pinimg.com/originals/48/9e/93/489e93437a3209ae36a2583c7ce562ad.jpg"]});
   	}
+    else if (message.content === 'quote'){
+        message.channel.send({files: ["https://i.pinimg.com/originals/b1/66/e8/b166e84e5f01e9ec56bc0f61a8ea940c.jpg"]});
+    }
 });
 
 // THIS  MUST  BE  THIS  WAY
