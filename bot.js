978b58e3f4861331b05227fae2c157c5f8af3cfb
Gianni Andries <45100198+GianniTM@users.noreply.github.com>
2020-05-21 20:50:43 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,18 +21,8 @@ client.on('message', message => {
         message.channel.send({files: ["https://i.pinimg.com/originals/b1/66/e8/b166e84e5f01e9ec56bc0f61a8ea940c.jpg"]});
     }
     else if (message.content === '/help'){
-	    const exampleEmbed = new Discord.MessageEmbed().setColor('#0099ff').setTitle('Some title').setURL('https://discord.js.org/').setAuthor('Some name', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org').setDescription('Some description here').setThumbnail('https://i.imgur.com/wSTFkRM.png').addFields(
-		{ name: 'Regular field title', value: 'Some value here' },
-		{ name: '\u200B', value: '\u200B' },
-		{ name: 'Inline field title', value: 'Some value here', inline: true },
-		{ name: 'Inline field title', value: 'Some value here', inline: true },
-	)
-	.addField('Inline field title', 'Some value here', true)
-	.setImage('https://i.imgur.com/wSTFkRM.png')
-	.setTimestamp()
-	.setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
+	    const exampleEmbed = new Discord.MessageEmbed().setTitle('Some title');
 
-	message.channel.send(exampleEmbed);
 });
 
 // THIS  MUST  BE  THIS  WAY
