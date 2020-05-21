cf2c5393111f74393f2f10160db4b8b33f7f8156
Gianni Andries <45100198+GianniTM@users.noreply.github.com>
2020-05-21 22:05:58 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -36,9 +36,6 @@ client.on('message', message => {
         if (!message.mentions.users.size) {
             return message.reply('you need to tag a user in order to kick them!');
         }
-        const taggedUser = message.mentions.users.first();
-
-        message.channel.send(You wanted to kick: ${taggedUser.username});
     }
 
 });
