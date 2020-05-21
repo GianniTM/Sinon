e6a8e0a303390a95c7f504a85a4ca0763bcdd4e6
Gianni Andries <45100198+GianniTM@users.noreply.github.com>
2020-05-21 19:49:55 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,13 +4,6 @@ const client = new Discord.Client();
 client.on('ready', () => {
     console.log('I am ready!');
      client.user.setStatus('dnd', 'Made by Xealius')
-     client.user.setPresence({
-        game: {
-            name: 'Pls Follow',
-            type: "STREAMING",
-            url: "https://www.twitch.tv/xealiusrl"
-        }
-    });
 });
 
 client.on('message', message => {
