48e74bd08afcbe9e45779f1b5d938a03815c9868
Gianni Andries <45100198+GianniTM@users.noreply.github.com>
2020-05-21 16:49:22 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,8 +6,8 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'Quote');
-    	message.reply('attachement');
+    if (message.content === 'ping') {
+    	message.reply('pong');
   	}
 });
 
