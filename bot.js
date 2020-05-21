b4d8b9f16654111aeb59edb82c4f5da785433782
Gianni Andries <45100198+GianniTM@users.noreply.github.com>
2020-05-21 16:07:39 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,8 +6,8 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
+    if (message.content === 'Quote') {
+    	message.reply('Whutt You Prevert!!');
   	}
 });
 
