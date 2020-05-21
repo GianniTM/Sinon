e55b91309b18973061b9c2b59e4001a9ddc4ce0b
Gianni Andries <45100198+GianniTM@users.noreply.github.com>
2020-05-21 21:59:51 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -30,16 +30,6 @@ client.on('message', message => {
     else if (message.content === `/server`) {
         message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
     }
-    else if (command === '/kick') {
-        // grab the "first" mentioned user from the message
-        // this will return a `User` object, just like `message.author`
-        if (!message.mentions.users.size) {
-            return message.reply('you need to tag a user in order to kick them!');
-        }
-        const taggedUser = message.mentions.users.first();
-
-        message.channel.send(`You wanted to kick: ${taggedUser.username}`);
-    }
 
 });
 
