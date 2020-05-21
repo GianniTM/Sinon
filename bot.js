9a63f2e603eba56fb041349ec04a5e982330261b
Jens Hermans <r0739255@thomasmore.be>
2020-05-21 21:28:56 +0200

    server test

--- bot.js
+++ bot.js
@@ -54,7 +54,7 @@ client.on('message', message => {
         message.channel.send("Testing embed 2");
         message.channel.send(exampleEmbed);
     }
-    else if (message.content === `${prefix}server`) {
+    else if (message.content === `/server`) {
         message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
     }
 });
