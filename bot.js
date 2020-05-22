a4f5b7133ce9f64ff314276c88a1b3c7029905ca
Gianni Andries <45100198+GianniTM@users.noreply.github.com>
2020-05-23 01:19:17 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -41,12 +41,12 @@ client.on('message', message => {
         message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
     }
     else if (message.content.startsWith ('/u')){
-        mention = message.mentions.users.first().id;
+        mention = message.mentions.users.first();
         if (mention == null){
             message.channel.send('pls tag someone');
             return;
         }
-        message.channel.send(<@{mention}>);
+        message.channel.send({mention});
     }
 
 
