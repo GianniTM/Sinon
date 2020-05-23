850ce5ddab8694abf78641f8d5547629cc93b156
Gianni Andries <45100198+GianniTM@users.noreply.github.com>
2020-05-23 22:55:47 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -62,11 +62,11 @@ client.on('message', message => {
     }
     else if (message.content === '/join'){
     // Only try to join the sender's voice channel if they are in one themselves
-    if (message.member.voice.channel) {
-      const connection = await message.member.voice.channel.join();
-    } else {
-      message.reply('You need to join a voice channel first!');
-    }
+        if (message.member.voice.channel) {
+            message.member.voice.channel.join();
+        } else {
+        message.reply('You need to join a voice channel first!');
+        }
     }
     
 });
