66e952a87ccbed4c578879bbbf6315399373b18e
Gianni Andries <r0739251@student.thomasmore.be>
2020-05-31 20:00:00 +0200

    rps

--- bot.js
+++ bot.js
@@ -267,19 +267,19 @@ client.on('message', async message => {
         var choiceNL = listNL[i];
         if(messages == 'rock' || messages == 'paper' || messages == 'scissors'){
             if(messages == 'rock' && choiceEN == 'rock'){
-                message.channel.send("**We tied!** I chose Rock.")
+                message.channel.send("**We tied!** I chose Rock.");
             }
             else if(messages == 'rock' && choiceEN == 'paper'){
-                message.channel.send("**I win!** I chose Paper.");
+                message.channel.send("**I won!** I chose Paper.");
             }
             else if(messages == 'rock' && choiceEN == 'scissors'){
-                message.channel.send("**You win!** i chose Scissors.");
+                message.channel.send("**You win!** I chose Scissors.");
             }
             else if(messages == 'paper' && choiceEN == 'rock'){
                 message.channel.send("**You win!** I chose Rock.");
             }
             else if(messages == 'scissors' && choiceEN == 'rock'){
-                message.channel.send("**I win!** I chose Rock.");
+                message.channel.send("**I won!** I chose Rock.");
             }
             else if(messages == 'paper' && choiceEN == 'paper'){
                 message.channel.send("**We tied!** I chose Paper.");
@@ -288,14 +288,44 @@ client.on('message', async message => {
                 message.channel.send("**I won!** I chose Scissors");
             }
             else if(messages == 'scissors' && choiceEN == 'scissors'){
-                message.channel.send("**We tied!** I chose Scissors.")
+                message.channel.send("**We tied!** I chose Scissors.");
+            }
+            else{
+                message.channel.send("Add another if not everything is implemented yet" + choiceEN);
             }
         }
         else if(messages == 'steen' || messages == 'papier' || messages == 'schaar'){
 
+            if(messages == 'steen' && choiceEN == 'steen'){
+                message.channel.send("**Het is gelijkspel!** Ik koos Steen.");
+            }
+            else if(messages == 'steen' && choiceEN == 'papier'){
+                message.channel.send("**Ik win!** Ik koos Papier.");
+            }
+            else if(messages == 'steen' && choiceEN == 'schaar'){
+                message.channel.send("**Jij wint!** Ik koos Schaar.");
+            }
+            else if(messages == 'papier' && choiceEN == 'steen'){
+                message.channel.send("**Jij wint!** Ik koos Steen.");
+            }
+            else if(messages == 'schaar' && choiceEN == 'steen'){
+                message.channel.send("**Ik win!** Ik koos Steen.");
+            }
+            else if(messages == 'papier' && choiceEN == 'papier'){
+                message.channel.send("**Het is gelijkspel!** Ik koos Papier.");
+            }
+            else if(messages == 'papier' && choiceEN == 'schaar'){
+                message.channel.send("**Ik win!** Ik koos Schaar");
+            }
+            else if(messages == 'schaar' && choiceEN == 'schaar'){
+                message.channel.send("**Het is gelijkspel!** Ik koos Schaar.");
+            }
+            else{
+                message.channel.send("Add another if not everything is implemented yet" + choiceNL);
+            }
         }
         else{
-            message.channel.send("You didn't schoose 1 of the options. The Languages you can play in are NL and EN");
+            message.channel.send("You didn't choose 1 of the options. The Languages you can play in are NL and EN");
         }
 
     }
