dbfa95efd7604c987559aaa9f2caed8c2ef6f5e7
Gianni Andries <r0739251@student.thomasmore.be>
2020-06-01 11:45:38 +0200

    music fix

--- bot.js
+++ bot.js
@@ -408,15 +408,20 @@ client.on('message', async message => {
         var server = servers[message.guild.id];
         if(!server.queue[0]){
             const embed = new Discord.RichEmbed();
-            embed.setAuthor("Now Playing:", message.author.displayAvatarURL);
+            embed.setAuthor("Queue:", message.author.displayAvatarURL);
             embed.setTitle("What are you looking at? There is nothing here!");
             message.channel.send({embed});
         }
         else{
+            const embed = new Discord.RichEmbed();
+            embed.setAuthor(message.author.displayAvatarURL);
+            embed.setAuthor("Queue:", message.author.displayAvatarURL);
             for(song of server.queue){
-                message.channel.send("Working on it don't worry");
+                embed.addField(song.title, song.channelTitle)
             }
+            message.channel.send({embed});
         }
+
     }
 
 });
