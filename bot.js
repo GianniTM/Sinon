a7b5c58e20af4b8efbf2fa50c58be8f8dccaca8d
Gianni Andries <r0739251@student.thomasmore.be>
2020-06-01 11:49:09 +0200

    music fix

--- bot.js
+++ bot.js
@@ -415,7 +415,7 @@ client.on('message', async message => {
         else{
             const embed = new Discord.RichEmbed();
             embed.setAuthor(message.author.displayAvatarURL);
-            embed.setAuthor("Queue:", message.author.displayAvatarURL);
+            embed.title("Queue:");
             for(song of server.queue){
                 embed.addField(song.title, song.channelTitle)
             }
