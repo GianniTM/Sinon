bbfd21031b9f969c9db6cb71d8acfe2366d78f99
Gianni Andries <r0739251@student.thomasmore.be>
2020-06-03 15:56:04 +0200

    feedback added

--- bot.js
+++ bot.js
@@ -458,7 +458,7 @@ client.on('message', async message => {
         embed.setThumbnail("https://images-ext-2.discordapp.net/external/C5rK2371x-fIsGosTVXQo1IzhaKIXpe6ol9Zgk8KrIw/%3Fsize%3D2048/https/cdn.discordapp.com/avatars/713003111945470013/0a883c7fe46b95b79b79e2e7a0021d5b.png?width=677&height=677");
         embed.setDescription(`React with the 🔫 emoji to partcipate!`);
         message.channel.send(embed).then(sentEmbed => {
-            sentEmbed.react("🔫 ");
+            sentEmbed.react("🔫");
         })
     }
 
