6528f1c5e23004e154cd6c2e6989359124f06393
Gianni Andries <r0739251@student.thomasmore.be>
2020-06-04 19:58:01 +0200

    resume/pause

--- bot.js
+++ bot.js
@@ -498,6 +498,20 @@ client.on('message', async message => {
                 }
                 return reaction.emoji.name === "🔫";
             };
+            setTimeout(function(){
+                embed.setTitle('**Russian Roulette**');
+                embed.setThumbnail("https://images-ext-2.discordapp.net/external/C5rK2371x-fIsGosTVXQo1IzhaKIXpe6ol9Zgk8KrIw/%3Fsize%3D2048/https/cdn.discordapp.com/avatars/713003111945470013/0a883c7fe46b95b79b79e2e7a0021d5b.png?width=677&height=677");
+                embed.setDescription(`Started by <@${message.author.id}>\nReact with the 🔫 emoji to partcipate!`);
+                embed.setFooter("Time remaining 10 seconds");
+                sentEmbed.edit(embed);
+            }, 5);
+            setTimeout(function(){
+                embed.setTitle('**Russian Roulette**');
+                embed.setThumbnail("https://images-ext-2.discordapp.net/external/C5rK2371x-fIsGosTVXQo1IzhaKIXpe6ol9Zgk8KrIw/%3Fsize%3D2048/https/cdn.discordapp.com/avatars/713003111945470013/0a883c7fe46b95b79b79e2e7a0021d5b.png?width=677&height=677");
+                embed.setDescription(`Started by <@${message.author.id}>\nReact with the 🔫 emoji to partcipate!`);
+                embed.setFooter("Time remaining 5 seconds");
+                sentEmbed.edit(embed);
+            }, 10);
             sentEmbed.awaitReactions(filter, { time: 15000 })
                 .then(collected => {var i = Math.floor(Math.random() * (participants.length - 1) + 1);
                     embed.setTitle('**Russian Roulette**');
