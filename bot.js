8a9ae13f0b113354e4405bab6a270609a99c98fc
Gianni Andries <r0739251@student.thomasmore.be>
2020-06-04 19:59:40 +0200

    resume/pause

--- bot.js
+++ bot.js
@@ -504,14 +504,14 @@ client.on('message', async message => {
                 embed.setDescription(`Started by <@${message.author.id}>\nReact with the 🔫 emoji to partcipate!`);
                 embed.setFooter("Time remaining 10 seconds");
                 sentEmbed.edit(embed);
-            }, 5);
+            }, 5000);
             setTimeout(function(){
                 embed.setTitle('**Russian Roulette**');
                 embed.setThumbnail("https://images-ext-2.discordapp.net/external/C5rK2371x-fIsGosTVXQo1IzhaKIXpe6ol9Zgk8KrIw/%3Fsize%3D2048/https/cdn.discordapp.com/avatars/713003111945470013/0a883c7fe46b95b79b79e2e7a0021d5b.png?width=677&height=677");
                 embed.setDescription(`Started by <@${message.author.id}>\nReact with the 🔫 emoji to partcipate!`);
                 embed.setFooter("Time remaining 5 seconds");
                 sentEmbed.edit(embed);
-            }, 10);
+            }, 10000);
             sentEmbed.awaitReactions(filter, { time: 15000 })
                 .then(collected => {var i = Math.floor(Math.random() * (participants.length - 1) + 1);
                     embed.setTitle('**Russian Roulette**');
